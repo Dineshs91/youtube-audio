9a6b8cd7b7216a6c68c07b9caa75e18c9e440344
dinesh <dineshpy07@gmail.com>
2017-05-30 23:52:32 +0530

    Split code into multiple files

---
+++ js/utils.js
@@ -0,0 +1,132 @@
+// Changes XML to JSON
+// https://davidwalsh.name/convert-xml-json
+function xmlToJson(xml) {
+    
+    // Create the return object
+    var obj = {};
+
+    if (xml.nodeType == 1) { // element
+        // do attributes
+        if (xml.attributes.length > 0) {
+        obj["@attributes"] = {};
+            for (var j = 0; j < xml.attributes.length; j++) {
+                var attribute = xml.attributes.item(j);
+                obj["@attributes"][attribute.nodeName] = attribute.nodeValue;
+            }
+        }
+    } else if (xml.nodeType == 3) { // text
+        obj = xml.nodeValue;
+    }
+
+    // do children
+    if (xml.hasChildNodes()) {
+        for(var i = 0; i < xml.childNodes.length; i++) {
+            var item = xml.childNodes.item(i);
+            var nodeName = item.nodeName;
+            if (typeof(obj[nodeName]) == "undefined") {
+                obj[nodeName] = xmlToJson(item);
+            } else {
+                if (typeof(obj[nodeName].push) == "undefined") {
+                    var old = obj[nodeName];
+                    obj[nodeName] = [];
+                    obj[nodeName].push(old);
+                }
+                obj[nodeName].push(xmlToJson(item));
+            }
+        }
+    }
+    return obj;
+};
+
+function formatParams(params) {
+  return "?" + Object
+        .keys(params)
+        .map(function(key) {
+          return key+"="+encodeURIComponent(params[key])
+        })
+        .join("&")
+}
+
+function QueryString(qs)
+{
+    this.dict= {};
+
+    // If no query string  was passed in use the one from the current page
+    if (!qs) qs= location.search;
+
+    // Delete leading question mark, if there is one
+    if (qs.charAt(0) == '?') qs= qs.substring(1);
+
+    // Parse it
+    var re= /([^=&]+)(=([^&]*))?/g;
+    while (match= re.exec(qs))
+    {
+        var key= decodeURIComponent(match[1].replace(/\+/g,' '));
+        var value= match[3] ? QueryString.decode(match[3]) : '';
+        if (this.dict[key])
+            this.dict[key].push(value);
+        else
+            this.dict[key]= [value];
+    }
+}
+
+QueryString.decode= function(s)
+{
+    s= s.replace(/\+/g,' ');
+    s= s.replace(/%([EF][0-9A-F])%([89AB][0-9A-F])%([89AB][0-9A-F])/gi,
+        function(code,hex1,hex2,hex3)
+        {
+            var n1= parseInt(hex1,16)-0xE0;
+            var n2= parseInt(hex2,16)-0x80;
+            if (n1 == 0 && n2 < 32) return code;
+            var n3= parseInt(hex3,16)-0x80;
+            var n= (n1<<12) + (n2<<6) + n3;
+            if (n > 0xFFFF) return code;
+            return String.fromCharCode(n);
+        });
+    s= s.replace(/%([CD][0-9A-F])%([89AB][0-9A-F])/gi,
+        function(code,hex1,hex2)
+        {
+            var n1= parseInt(hex1,16)-0xC0;
+            if (n1 < 2) return code;
+            var n2= parseInt(hex2,16)-0x80;
+            return String.fromCharCode((n1<<6)+n2);
+        });
+    s = s.replace(/%([0-7][0-9A-F])/gi,
+        function(code,hex)
+        {
+            return String.fromCharCode(parseInt(hex,16));
+        });
+    return s;
+};
+
+QueryString.prototype.value= function (key)
+{
+    var a= this.dict[key];
+    return a ? a[a.length-1] : undefined;
+};
+
+QueryString.prototype.values= function (key)
+{
+    var a= this.dict[key];
+    return a ? a : [];
+};
+
+QueryString.prototype.keys= function ()
+{
+    var a= [];
+    for (var key in this.dict)
+        a.push(key);
+    return a;
+};
+
+// https://stackoverflow.com/a/901144/2134124
+function getParameterByName(name, url) {
+    if (!url) url = window.location.href;
+    name = name.replace(/[\[\]]/g, "\\$&");
+    var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
+        results = regex.exec(url);
+    if (!results) return null;
+    if (!results[2]) return '';
+    return decodeURIComponent(results[2].replace(/\+/g, " "));
+}
