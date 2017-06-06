20d47a788ece7359f004785406a8930630e69eb8
dinesh <dineshpy07@gmail.com>
2017-06-06 12:08:18 +0530

    Send the url in the message from background to content script

--- js/background.js
+++ js/background.js
@@ -1,5 +1,6 @@
 chrome.webNavigation.onHistoryStateUpdated.addListener(function(details) {
     var tabId = details.tabId;
+    console.log(details);
 
-    chrome.tabs.sendMessage(tabId, {"text": "start"}, function(response) { console.log(response) });
+    chrome.tabs.sendMessage(tabId, {"text": "start", "url": details.url}, function(response) { console.log(response) });
 });
