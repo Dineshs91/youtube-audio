8caeada070604d007d2150b931de22b5a08dfac3
dinesh <dineshpy07@gmail.com>
2017-06-11 08:45:04 +0530

    Add comments to background.js file

--- js/background.js
+++ js/background.js
@@ -1,38 +1,33 @@
-chrome.webNavigation.onHistoryStateUpdated.addListener(function(details) {
-    var tabId = details.tabId;
-    console.log(details);
+/*
+Decrypt the signature, so that it can be used to get the manifest without hitting 403.
+*/
+function handle_decryption(msg) {
+    console.log("Received message")
+    var manifestUrl = msg.manifestUrl;
+    var playerUrl = msg.playerUrl;
+    var encryptedCode = msg.encryptedCode;
 
-    chrome.tabs.sendMessage(tabId, {"text": "start", "url": details.url}, function() {});
-});
+    $.get(playerUrl, function(playerSrcCode) {
+        var sigFunction = get_function(playerSrcCode);
+        var decryptSrcCode = find_decrypt_src_code(playerSrcCode, sigFunction);
 
-chrome.runtime.onMessage.addListener(
-    function(msg, sender, sendResponse) {
-        // This will take the function, and the code to be decrypted.
-        if (msg.playerUrl) {
-            console.log("Received message")
-            var manifestUrl = msg.manifestUrl;
-            var playerUrl = msg.playerUrl;
-            var encryptedCode = msg.encryptedCode;
-
-            $.get(playerUrl, function(playerSrcCode) {
-                var sigFunction = get_function(playerSrcCode);
-                var decryptSrcCode = find_decrypt_src_code(playerSrcCode, sigFunction);
-
-                chrome.tabs.executeScript(null, {
-                    code: decryptSrcCode + sigFunction + "('" + encryptedCode + "')"
-                }, function(result) {
-                    chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
-                        chrome.tabs.sendMessage(tabs[0].id, {
-                            manifestUrl: manifestUrl,
-                            decryptedCode: result
-                        }, function() {});
-                    });
-                });
+        chrome.tabs.executeScript(null, {
+            code: decryptSrcCode + sigFunction + "('" + encryptedCode + "')"
+        }, function(result) {
+            chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
+                chrome.tabs.sendMessage(tabs[0].id, {
+                    manifestUrl: manifestUrl,
+                    decryptedCode: result
+                }, function() {});
             });
-        }
-    }
-)
+        });
+    });
+}
 
+/*
+Get the decrypt src code (JS), which includes the decrypting function and its dependencies.
+This code will be executed using chrome.tabs.executeScript.
+*/
 function find_decrypt_src_code(srcCode, sigFunction) {
     var regexStr = "(?:function\\s+" + sigFunction + "|[{;,]\\s*" + sigFunction + "\\s*=\\s*function|var\\s+" + sigFunction + "\\s*=\\s*function)\\s*\\(([^)]*)\\)\\s*\\{([^}]+)\\};";
 
@@ -58,6 +53,9 @@ function find_decrypt_src_code(srcCode, sigFunction) {
     return decryptSrcCode;
 }
 
+/*
+Get the dependent functions of the decrypting function.
+*/
 function get_dependent_function(srcCode, depFuncName) {
     var pattern = new RegExp("var\\s+" + depFuncName + "={([^]+?)};");
     var mobj = pattern.exec(srcCode);
@@ -70,6 +68,9 @@ function get_dependent_function(srcCode, depFuncName) {
     return depFunc;
 }
 
+/*
+Get the function from base/player js file which will decrypt the signature.
+*/
 function get_function(srcCode) {
     var pattern = new RegExp(/([\"\'])signature\1\s*,\s*([a-zA-Z0-9$]+)\(/);
     var mobj = pattern.exec(srcCode);
@@ -81,3 +82,28 @@ function get_function(srcCode) {
 
     return sigFunction;
 }
+
+/////////////////////////////////////////// Listeners ///////////////////////////////////////////
+
+/*
+Listen for history changes. This is required since yt is an ajax site and to identify
+the page navigation this is the only way to detect these changes.
+*/
+chrome.webNavigation.onHistoryStateUpdated.addListener(function(details) {
+    var tabId = details.tabId;
+    console.log(details);
+
+    chrome.tabs.sendMessage(tabId, {"text": "start", "url": details.url}, function() {});
+});
+
+/*
+Message listeners.
+*/
+chrome.runtime.onMessage.addListener(
+    function(msg, sender, sendResponse) {
+        if (msg.playerUrl) {
+            // Decrypt the codes
+            handle_decryption(msg);
+        }
+    }
+)
