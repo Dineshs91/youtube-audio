ddfeffaa3fbf5ebed4b33542eb0aef5ed33a67c7
dinesh <dineshpy07@gmail.com>
2017-06-11 13:44:02 +0530

    Initial refactor architecture
    Code which manipulates or requires dom should be in content script.
    All other code should be in background script

--- js/background/background.js
+++ js/background/background.js
@@ -1,13 +1,28 @@
+var dashMpds = [];
+
+function send_message(tabId, msg) {
+    chrome.tabs.sendMessage(tabId, msg, function() {});
+}
+
 /////////////////////////////////////////// Decrypt signature ///////////////////////////////////////////
+/*
+Replace /s/ADFEB.EBEB/ => /signature/EDADD.CDC/
+*/
+function form_decrypted_manifest_url(manifestUrl, decryptedCode) {
+    var decryptedManifestUrl = null;
+    var pattern = new RegExp(/\/s\/([a-fA-F0-9\.]+)\//);
+    var value = "/signature/" + decryptedCode + "/"
+    
+    var decryptedManifestUrl = manifestUrl.replace(pattern, value);
+    return decryptedManifestUrl;
+}
 
 /*
-Decrypt the signature, so that it can be used to get the manifest without hitting 403.
+Decrypt the signature, so that it can be used to get the manifest without getting 403.
 */
-function handle_decryption(msg) {
-    console.log("Received message")
-    var manifestUrl = msg.manifestUrl;
-    var playerUrl = msg.playerUrl;
-    var encryptedCode = msg.encryptedCode;
+function get_decrypted_manifest_url(manifestUrl, playerUrl, encryptedCode) {
+    var deferred = Q.defer();
+    console.log("[youtube-audio]  Received message")
 
     $.get(playerUrl, function(playerSrcCode) {
         var sigFunction = get_function(playerSrcCode);
@@ -15,15 +30,15 @@ function handle_decryption(msg) {
 
         chrome.tabs.executeScript(null, {
             code: decryptSrcCode + sigFunction + "('" + encryptedCode + "')"
-        }, function(result) {
-            chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
-                chrome.tabs.sendMessage(tabs[0].id, {
-                    manifestUrl: manifestUrl,
-                    decryptedCode: result
-                }, function() {});
-            });
+        }, function(decryptedCode) {
+            var decryptedManifestUrl = form_decrypted_manifest_url(manifestUrl, decryptedCode);
+            deferred.resolve(decryptedManifestUrl);
         });
+    }).fail(function(jqXHR, textStatus, errorThrown) {
+        deferred.reject(errorThrown);
     });
+
+    return deferred.promise;
 }
 
 /*
@@ -85,6 +100,262 @@ function get_function(srcCode) {
     return sigFunction;
 }
 
+/////////////////////////////////////////// start ///////////////////////////////////////////
+/*
+Get thumbnail for the video.
+*/
+function get_thumbnail(webpage) {
+    var pattern = new RegExp(/<link itemprop="thumbnailUrl".*?href="(.*?)">/);
+    var mobj = pattern.exec(webpage);
+    var thumbnailUrl = "";
+
+    if (mobj != null) {
+        thumbnailUrl = mobj[1];
+    }
+
+    return thumbnailUrl;
+}
+
+/*
+Get ytplayer config from the webpage.
+*/
+function get_ytplayer_config(webpage) {
+    patterns = [new RegExp(/;ytplayer\.config\s*=\s*({.+?});ytplayer/),
+                new RegExp(/;ytplayer\.config\s*=\s*({.+?});/)];
+
+    for (var i = 0; i < patterns.length; i++) {
+        var pattern = patterns[i];
+        var found = pattern.exec(webpage);
+
+        if (found != null || found != undefined) {
+            var ytplayer_config = found[1];
+            return JSON.parse(ytplayer_config);
+        }
+    }
+}
+
+/*
+Extract video info from the webpage.
+*/
+function get_video_info(videoId, webpage) {
+    var deferred = Q.defer();
+    var ytplayerConfig = get_ytplayer_config(webpage);
+    var videoInfoUrl = "https://www.youtube.com/get_video_info/";
+
+    var sts = null;
+    var videoInfo = null;
+
+    if (ytplayerConfig != null) {
+        var args = ytplayerConfig['args'];
+
+        if (args['url_encoded_fmt_stream_map'] != null || args['url_encoded_fmt_stream_map'] != undefined) {
+            videoInfo = args;
+            console.log("[youtube-audio] Obtained video info from ytplayer config ");
+            add_dash_mpd(videoInfo);
+            sts = ytplayerConfig['sts'];
+            if(videoInfo != null && videoInfo['dashmpd'] != null) {
+                deferred.resolve(videoInfo);    
+            }
+        }
+    }
+    if (videoInfo == null || dashMpds.length == 0) {
+        var el_types = ["info", "embedded", "detailpage", "vevo", ""];
+
+        for(var i = 0; i < el_types.length; i++) {
+            var query = {
+                'video_id': videoId,
+                'ps': 'default',
+                'eurl': '',
+                'gl': 'US',
+                'hl': 'en',
+            };
+            var el = el_types[i];
+            if (el != "") {
+                query["el"] = el;
+            }
+
+            if (sts != null || sts != undefined || sts != "") {
+                query["sts"] = sts;
+            }
+
+            // Get the video info.
+            $.get(videoInfoUrl, query, function(data) {
+                var qs = new QueryString(data);
+                var dashMpd = qs.value("dashmpd");
+
+                if (dashMpd != null || dashMpd != undefined) {
+                    var videoInfo = {"dashmpd": dashMpd};
+                    deferred.resolve(videoInfo);
+                }
+            }).fail(function(jqXHR, textStatus, errorThrown) {
+                deferred.reject(errorThrown);
+            });
+        }
+    }
+
+    return deferred.promise;
+}
+
+/*
+Add dash mpd links to dashMpds array.
+*/
+function add_dash_mpd(videoInfo) {
+    var dashMpd = videoInfo['dashmpd'];
+
+    if (dashMpd != null || dashMpd != undefined) {
+        var duplicateFound = false;
+        for(var i = 0; i < dashMpds.length; i++) {
+            if(dashMpds[i] == dashMpd) {
+                duplicateFound = true;
+                break;
+            }
+        }
+        if (duplicateFound == false) {
+            dashMpds.push(dashMpd);
+        }
+    }
+}
+
+/*
+manifest.googlevideo.com/api/manifest/dash/source/youtube/s/9EF04C53DC3BA02F8622B1A2C7205EAF1DA5EB.E1F1766EF29E9AD8AA9D496D9FDF4EC298D620E/pl/24/.../requiressl/yes
+
+/s/{encryptedCode}/
+*/
+function get_encrypted_code_from_manifest(manifestUrl) {
+    var pattern = new RegExp(/\/s\/([a-fA-F0-9\.]+)/);
+    var mobj = pattern.exec(manifestUrl);
+
+    var encryptedCode = null;
+    if (mobj != null) {
+        encryptedCode = mobj[1];
+    }
+
+    return encryptedCode;
+}
+
+/*
+Convert all manifest url's containing encrypted codes to url's with decrypted codes.
+*/
+function convert_to_decrypted_manifest_urls(playerUrl) {
+    var deferred = Q.defer();
+    var decryptedManifestPromises = [];
+
+    for (var i = 0; i < dashMpds.length; i++) {
+        var manifestUrl = dashMpds[i];
+        var encryptedCode = get_encrypted_code_from_manifest(manifestUrl);
+
+        decryptedManifestPromises.push(get_decrypted_manifest_url(manifestUrl, playerUrl, encryptedCode));
+    }
+    Q.all(decryptedManifestPromises).then(function(decryptedManifestUrls) {
+        deferred.resolve(decryptedManifestUrls)
+    }).catch(function(error) {
+        deferred.reject(error);
+    });
+
+    return deferred.promise;
+}
+
+/*
+This function tries to return atleast one successful dash manifest.
+*/
+function get_dash_manifest(decryptedDashManifests) {
+    var deferred = Q.defer();
+    var success = false;
+    var j = 0;
+
+    for (var i = 0; i < decryptedDashManifests.length; i++) {
+        var manifestUrl = decryptedDashManifests[i];
+        $.get(manifestUrl, function(data) {
+            success = true;
+            deferred.resolve(data);
+        }).fail(function(jqXHR, textStatus, errorThrown) {
+            j += 1;
+            if (success == false && j == decryptedDashManifests.length - 1) {
+                deferred.reject(errorThrown);    
+            }
+        });
+    }
+
+    return deferred.promise;
+}
+
+/*
+parse xml to json and return the object.
+*/
+function xml_parse(doc) {
+    parser = new DOMParser();
+    xmlDoc = parser.parseFromString(doc, "text/xml");
+
+    jsonDoc = xmlToJson(xmlDoc);
+    return jsonDoc;
+}
+
+/*
+Return audio/mp4 adaption set.
+*/
+function get_audio_adaption_set(adaptionSets) {
+    for(var i = 0; i < adaptionSets.length; i++) {
+        if(adaptionSets[i]['@attributes']['mimeType'] == "audio/mp4") {
+            return adaptionSets[i];
+        }
+    }
+}
+
+/*
+Return a list of all audio formats available.
+*/
+function get_audio_links(jsonDoc) {
+    var audioLinks = [];
+
+    if (jsonDoc.hasOwnProperty('MPD')) {
+        var adaptionSets = jsonDoc['MPD']['Period']['AdaptationSet'];
+        var audioAdaptionSet = get_audio_adaption_set(adaptionSets);
+
+        var audioRepresentations = audioAdaptionSet['Representation'];
+        for(var i = 0; i < audioRepresentations.length; i++) {
+            var audioRepresentation = audioRepresentations[i];
+            audioLinks.push({
+                "audioSamplingRate": audioRepresentation['@attributes']['audioSamplingRate'],
+                "bandwidth": audioRepresentation['@attributes']['bandwidth'],
+                "link": audioRepresentation['BaseURL']['#text']
+            });
+        }
+    }
+
+    console.log("[youtube-audio] Found [" + audioLinks.length + "] audio links");
+    return audioLinks;
+}
+
+function start(tabId, msg) {
+    // clear dashMpds array before starting.
+    dashMpds = [];
+
+    var webpage = msg.webpage;
+    var videoId = msg.videoId;
+    var baseJs = msg.baseJs;
+    var playerUrl = "https://www.youtube.com" + baseJs;
+
+    get_video_info(videoId, webpage).then(function(videoInfo) {
+        add_dash_mpd(videoInfo);
+        return convert_to_decrypted_manifest_urls(playerUrl);
+    }).then(function(decryptedDashManifests) {
+        return get_dash_manifest(decryptedDashManifests)
+    }).then(function(dashManifest) {
+        var jsonDoc = xml_parse(dashManifest);
+        var audioLink = get_audio_links(jsonDoc)[0]['link'];
+        var thumbnailUrl = get_thumbnail(webpage);
+
+        var msg = {
+            type: constant.audioFound,
+            audioLink: audioLink,
+            thumbnailUrl: thumbnailUrl
+        }
+        send_message(tabId, msg);
+    }).catch(function(error) {
+        console.log("Some error occurred: " + error);
+    });
+}
+
 /////////////////////////////////////////// Listeners ///////////////////////////////////////////
 
 /*
@@ -93,9 +364,14 @@ the page navigation this is the only way to detect these changes.
 */
 chrome.webNavigation.onHistoryStateUpdated.addListener(function(details) {
     var tabId = details.tabId;
-    console.log(details);
+    //console.log(details);
 
-    chrome.tabs.sendMessage(tabId, {"text": "start", "url": details.url}, function() {});
+    msg = {
+        type: constant.historyChange,
+        url: details.url
+    }
+
+    chrome.tabs.sendMessage(tabId, msg, function() {});
 });
 
 /*
@@ -103,9 +379,17 @@ Message listeners.
 */
 chrome.runtime.onMessage.addListener(
     function(msg, sender, sendResponse) {
-        if (msg.playerUrl) {
-            // Decrypt the codes
-            handle_decryption(msg);
+        var tabId = sender.tab.id;
+
+        if (msg.type === constant.videoFound) {
+            // msg = {
+            //     type: videoFound
+            //     videoId: videoId,
+            //     webpage: webpage,
+            //     baseJs: baseJs
+            // }
+
+            start(tabId, msg);
         }
     }
 )
--- js/youtube.js
+++ js/youtube.js
@@ -1,131 +1,11 @@
-// dashMpds contains a collection of manifest urls.
-// Accessing the manifest url gives an xml file, with the audio and video links in available formats.
-var dashMpds = [];
 var autoplayEnabled = false;
 var enableObserver = false;
-var observer;
-var videoInfoUrl = "https://www.youtube.com/get_video_info/";
-
-/*
-Get ytplayer config from the webpage.
-*/
-function get_ytplayer_config(webpage) {
-    patterns = [new RegExp(/;ytplayer\.config\s*=\s*({.+?});ytplayer/),
-                new RegExp(/;ytplayer\.config\s*=\s*({.+?});/)];
-
-    for (var i = 0; i < patterns.length; i++) {
-        var pattern = patterns[i];
-        var found = pattern.exec(webpage);
-
-        if (found != null || found != undefined) {
-            var ytplayer_config = found[1];
-            return JSON.parse(ytplayer_config);
-        }
-    }
-}
 
 /*
 Get the video id of the current page.
 */
 function get_video_id() {
     return getParameterByName('v');
-}
-
-/*
-Extract video info from the webpage.
-*/
-function get_video_info(webpage) {
-    var deferred = Q.defer();
-    var ytplayerConfig = get_ytplayer_config(webpage);
-    var videoInfo = null;
-    var sts = null;
-
-    if (ytplayerConfig != null) {
-        var args = ytplayerConfig['args'];
-
-        if (args['url_encoded_fmt_stream_map'] != null || args['url_encoded_fmt_stream_map'] != undefined) {
-            videoInfo = args;
-            console.log("Obtained video info from ytplayer config ");
-            add_dash_mpd(videoInfo);
-            sts = ytplayerConfig['sts'];
-            if(videoInfo != null && videoInfo['dashmpd'] != null) {
-                deferred.resolve(videoInfo);    
-            }
-        }
-    }
-    if (videoInfo == null || dashMpds.length == 0) {
-        var el_types = ["info", "embedded", "detailpage", "vevo", ""];
-
-        for(var i = 0; i < el_types.length; i++) {
-            var query = {
-                'video_id': get_video_id(),
-                'ps': 'default',
-                'eurl': '',
-                'gl': 'US',
-                'hl': 'en',
-            };
-            var el = el_types[i];
-            if (el != "") {
-                query["el"] = el;
-            }
-
-            if (sts != null || sts != undefined || sts != "") {
-                query["sts"] = sts;
-            }
-
-            // Get the video info.
-            $.get(videoInfoUrl, query, function(data) {
-                var qs = new QueryString(data);
-                var dashMpd = qs.value("dashmpd");
-
-                if (dashMpd != null || dashMpd != undefined) {
-                    var videoInfo = {"dashmpd": dashMpd};
-                    deferred.resolve(videoInfo);
-                }
-            }).fail(function(jqXHR, textStatus, errorThrown) {
-                deferred.reject(errorThrown);
-            });
-        }
-    }
-
-    return deferred.promise;
-}
-
-/*
-Get all the audio links from the array of dashMpds.
-*/
-function get_dash_manifest() {
-    var deferred = Q.defer();
-
-    for (var i = 0; i < dashMpds.length; i++) {
-        var manifestUrl = dashMpds[i];
-        $.get(manifestUrl, function(data) {
-            deferred.resolve(data);
-        }).fail(function(jqXHR, textStatus, errorThrown) {
-            // Try to download the decrypted manifest.
-            decrypt_manifest_signature(manifestUrl);
-            deferred.reject(errorThrown);
-        });
-    }
-
-    return deferred.promise;
-}
-
-/*
-manifest.googlevideo.com/api/manifest/dash/source/youtube/s/9EF04C53DC3BA02F8622B1A2C7205EAF1DA5EB.E1F1766EF29E9AD8AA9D496D9FDF4EC298D620E/pl/24/.../requiressl/yes
-
-/s/{encryptedCode}/
-*/
-function get_encrypted_code_from_manifest(manifestUrl) {
-    var pattern = new RegExp(/\/s\/([a-fA-F0-9\.]+)/);
-    var mobj = pattern.exec(manifestUrl);
-
-    var encryptedCode = null;
-    if (mobj != null) {
-        encryptedCode = mobj[1];
-    }
-
-    return encryptedCode;
 }
 
 /*
@@ -140,116 +20,22 @@ function get_base_js() {
     return baseJs;
 }
 
-/*
-playerUrl: https://www.youtube.com/yts/jsbin/player-vflZ_L_3c/en_US/base.js
-*/
-function decrypt_manifest_signature(manifestUrl) {
-    var playerUrl = "https://www.youtube.com" + get_base_js();
-    var encryptedCode = get_encrypted_code_from_manifest(manifestUrl);
-    console.log("Get decrypted manifest signature");
-
-    // Handle in background script.
-    chrome.runtime.sendMessage({
-        manifestUrl: manifestUrl,
-        playerUrl: playerUrl,
-        encryptedCode: encryptedCode
-    }, function() {});
-}
-
-function add_dash_mpd(videoInfo) {
-    var dashMpd = videoInfo['dashmpd'];
-
-    if (dashMpd != null || dashMpd != undefined) {
-        var duplicateFound = false;
-        for(var i = 0; i < dashMpds.length; i++) {
-            if(dashMpds[i] == dashMpd) {
-                duplicateFound = true;
-                break;
-            }
-        }
-        if (duplicateFound == false) {
-            dashMpds.push(dashMpd);
-        }
-    }
-}
-
-/*
-parse xml to json and return the object.
-*/
-function xml_parse(doc) {
-    parser = new DOMParser();
-    xmlDoc = parser.parseFromString(doc, "text/xml");
-
-    jsonDoc = xmlToJson(xmlDoc);
-    return jsonDoc;
-}
-
-/*
-Return audio/mp4 adaption set.
-*/
-function get_audio_adaption_set(adaptionSets) {
-    for(var i = 0; i < adaptionSets.length; i++) {
-        if(adaptionSets[i]['@attributes']['mimeType'] == "audio/mp4") {
-            return adaptionSets[i];
-        }
-    }
-}
-
-/*
-Return a list of all audio formats available.
-
-TODO: Fetch all, currently fetches only the first one.
-*/
-function get_audio_links(jsonDoc) {
-    var audioLinks = [];
-
-    if (jsonDoc.hasOwnProperty('MPD')) {
-        var adaptionSets = jsonDoc['MPD']['Period']['AdaptationSet'];
-        var audioAdaptionSet = get_audio_adaption_set(adaptionSets);
-
-        var audioRepresentations = audioAdaptionSet['Representation'];
-        for(var i = 0; i < audioRepresentations.length; i++) {
-            var audioRepresentation = audioRepresentations[i];
-            audioLinks.push({
-                "audioSamplingRate": audioRepresentation['@attributes']['audioSamplingRate'],
-                "bandwidth": audioRepresentation['@attributes']['bandwidth'],
-                "link": audioRepresentation['BaseURL']['#text']
-            });
-        }
-    }
-
-    console.log("Found [" + audioLinks.length + "] audio links");
-    return audioLinks;
-}
-
-function get_thumbnail(webpage, videoInfo) {
-    var pattern = new RegExp(/<link itemprop="thumbnailUrl".*?href="(.*?)">/);
-    var mobj = pattern.exec(webpage);
-    var thumbnailUrl = "";
-
-    if (mobj != null) {
-        thumbnailUrl = mobj[1];
-    } else if(!(videoInfo == null || videoInfo == undefined) && !(videoInfo['thumbnail_url'] == null || videoInfo['thumbnail_url'] == undefined)) {
-        thumbnailUrl = videoInfo['thumbnail_url'];
-    }
-
-    return thumbnailUrl;
-}
-
 function play_next_audio() {
     var nextAudioLink = $("#watch7-sidebar-modules .watch-sidebar-section").get(0);
     nextAudioLink = $(nextAudioLink).find(".watch-sidebar-body .content-link").get(0);
 
     if (nextAudioLink != null || nextAudioLink != undefined) {
         nextAudioLink.click();
-        console.log("Playing next audio");
+        console.log("[youtube-audio] Playing next audio");
     } else {
-        console.log("Unable to get the next audio.");
+        console.log("[youtube-audio] Unable to get the next audio.");
     }
 }
 
 function embed_audio_to_webpage(audioLink, thumbnailUrl) {
-    console.log("Embedding custom video element");
+    remove_custom_video_elements();
+
+    console.log("[youtube-audio] Embedding custom video element");
     var videoElement = create_video_element(audioLink);
     $("#player-api").append("<div class='html5-video-player audiox'></div>");
     $(".html5-video-player").prepend(videoElement);
@@ -293,7 +79,7 @@ function remove_custom_video_elements() {
 Remove youtube's video element. 
 */
 function remove_video_elements() {
-    console.log("Removing youtube video elements");
+    console.log("[youtube-audio]  Removing youtube video elements");
 
     // Copy the element before removing
     var videoPlayer = $("#movie_player");
@@ -326,15 +112,15 @@ function add_event_listeners() {
         if (enableObserver && $(this)[0].className == "html5-video-player audiox") {
             // console.log($(this)[0].className);
             if (! $(".html5-video-player.audiox").length) {
-                console.log("Trying to embed again");
-                start();
+                console.log("[youtube-audio]  Trying to embed again");
+                init();
             }
         }
     });
 
     // Autoplay
     $(".audiox").on("ended", function() {
-        console.log("Autoplay enabled: " + autoplayEnabled);
+        console.log("[youtube-audio] Autoplay enabled: " + autoplayEnabled);
         if(autoplayEnabled) {
             play_next_audio();
         }
@@ -344,7 +130,7 @@ function add_event_listeners() {
     // pause the video and remove it first. (except for our video element.)
     $('#player-api').arrive("video", function() {
         if(enableObserver && $(this)[0].className != "html5-video-player audiox" ) {
-            $(this).pause();
+            $(this).trigger('pause');
             $(this).remove();
         }
     });
@@ -362,141 +148,72 @@ function get_webpage() {
     return document.body.innerHTML;
 }
 
-function start() {
-    // Clear any previous state.
-    dashMpds = [];
-    autoplayEnabled = false;
-    enableObserver = false;
+function send_message(msg) {
+    chrome.runtime.sendMessage(msg, function() {});
+}
 
-    remove_custom_video_elements();
+function handle_audio_found(msg) {
+    var audioLink = msg.audioLink;
+    var thumbnailUrl = msg.thumbnailUrl;
 
-    var videoId = get_video_id();
+    if (audioLink != null || audioLink != undefined || audioLink != "") {
+        enableObserver = true;
+        remove_video_elements();
+        embed_audio_to_webpage(audioLink, thumbnailUrl);
 
-    if (videoId == null || videoId == undefined || videoId == "") {
-        console.log("video id not found");
-        return;
+        autoplay_enabled();
     } else {
-        console.log("Found video id: " + videoId);
+        enableObserver = false;
     }
 
-    var webpage = get_webpage();
-    var thumbnailUrl = "";
-    var audioLink;
+    add_event_listeners();
+}
 
-    get_video_info(webpage).then(function(videoInfo) {
-        console.log("Obtained video info");
-        add_dash_mpd(videoInfo);
-        thumbnailUrl = get_thumbnail(webpage, videoInfo);
-        return get_dash_manifest();
-    }).then(function(dashManifest) {
-        console.log("Retreived dash manifest successfully");
-        jsonDoc = xml_parse(dashManifest);
-        audioLink = get_audio_links(jsonDoc)[0]['link'];
+function init() {
+    var videoId = get_video_id();
+    if (videoId != null || videoId != undefined || videoId === "") {
+        console.log("[youtube-audio] found video:" + videoId);
+        var webpage = get_webpage();
+        var baseJs = get_base_js();
 
-        if (audioLink != null || audioLink != undefined || audioLink != "") {
-            enableObserver = true;
-            remove_video_elements();
-            embed_audio_to_webpage(audioLink, thumbnailUrl);
-
-            autoplay_enabled();
+        var msg = {
+            type: constant.videoFound,
+            videoId: videoId,
+            webpage: webpage,
+            baseJs: baseJs
         }
-    }).catch(function(e) {
-        enableObserver = false;
-        console.log(e);
-    }).fin(function() {
-        console.log("Adding event listeners");
-        add_event_listeners();
-    });
+
+        send_message(msg);
+    }
 }
 
-/*
-Replace /s/ADFEB.EBEB/ => /signature/EDADD.CDC/
-*/
-function form_decrypted_manifest_url(manifestUrl, decryptedCode) {
-    var decryptedManifestUrl = null;
-    var pattern = new RegExp(/\/s\/([a-fA-F0-9\.]+)\//);
-    var value = "/signature/" + decryptedCode + "/"
-    
-    var decryptedManifestUrl = manifestUrl.replace(pattern, value);
-    return decryptedManifestUrl;
-}
+//init();
 
-/*
-This manifest url should contain decrypted signatures.
-*/
-function fetch_decrypted_manifest(decryptedManifestUrl) {
-    var deferred = Q.defer();
+/////////////////////////////////////////// Listeners ///////////////////////////////////////////
 
-    $.get(decryptedManifestUrl, function(data) {
-        deferred.resolve(data);
-    }).fail(function(jqXHR, textStatus, errorThrown) {
-        deferred.reject(errorThrown)
-    });
-
-    return deferred.promise;
-}
-
-/*
-Handle decrypted manifest flow.
-*/
-function handle_decrypted_manifest(manifestUrl, decryptedCode) {
-    console.log("handle decrypted manifest");
-    var webpage = get_webpage();
-    var audioLink = null;
-
-
-    var thumbnailUrl = get_thumbnail(webpage);
-    var decryptedManifestUrl = form_decrypted_manifest_url(manifestUrl, decryptedCode);
-    fetch_decrypted_manifest(decryptedManifestUrl).then(function(dashManifest) {
-        console.log("Retreived dash manifest successfully by decrypting");
-        jsonDoc = xml_parse(dashManifest);
-        audioLink = get_audio_links(jsonDoc)[0]['link'];
-
-        if (audioLink != null || audioLink != undefined || audioLink != "") {
-            enableObserver = true;
-            remove_video_elements();
-            embed_audio_to_webpage(audioLink, thumbnailUrl);
-
-            autoplay_enabled();
-        }
-    }).catch(function(e) {
-        enableObserver = false;
-        console.log(e);
-    }).fin(function() {
-        console.log("Adding event listeners");
-        add_event_listeners();
-    });
-}
-
-/*
-Listen to start message from background page. It gets triggered from chrome.webNavingation event.
-*/
 chrome.runtime.onMessage.addListener(function(msg, sender, sendResponse) {
-    /* If the received message has the expected format... */
-    if (msg.text && (msg.text == 'start')) {
-        console.log('Received a msg from background page...');
+    if(msg.type == constant.audioFound) {
+        // msg = {
+        //     type: audioFound,
+        //     audioLink: audioLink,
+        //     thumbnailUrl: thumbnailUrl
+        // };
 
-        // check if element is present.
-        if ($("#eow-title").length) {
-            var v = getParameterByName("v", msg.url);
+        console.log("[youtube-audio] Found audio");
+        handle_audio_found(msg);
+    } else if(msg.type == constant.historyChange) {
+        // msg = {
+        //     type: historyChange,
+        //     url: url
+        // };
 
-            if(get_video_id() == v) {
-                start();
-            }
-        } else {
-            $(document).arrive("#eow-title", function() {
-                var v = getParameterByName("v", msg.url);
-                if(get_video_id() == v) {
-                    $(document).unbindArrive("#eow-title");
-                    start();
-                }
-            });
+        console.log("[youtube-audio] history change");
+        var url = msg.url;
+        var historyVideoId = get_video_id(url);
+        var currentVideoId = get_video_id();
+
+        if (currentVideoId === historyVideoId) {
+            init();    
         }
-    } else if(msg.decryptedCode) {
-        console.log("Received decrypted code");
-        var decryptedCode = msg.decryptedCode[0];
-        var manifestUrl = msg.manifestUrl;
-
-        handle_decrypted_manifest(manifestUrl, decryptedCode);
     }
 });
